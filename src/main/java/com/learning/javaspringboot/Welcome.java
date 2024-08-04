de68b9550cd42e6ed81c018cc1bc7f004be1a79e
YuriiShchyrba <yurashchyrba@gmail.com>
2024-08-04 17:48:08 -0400

    change name

--- src/main/java/com/learning/javaspringboot/Welcome.java
+++ src/main/java/com/learning/javaspringboot/Welcome.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWorld {
+public class Welcome {
     @GetMapping("welcome")
-    public String helloWorld() {
+    public String welcome() {
         return "Hello Juan Contreras!";
     }
 }
